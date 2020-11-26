fbbea0f697856c2a9e131d765338eef45bec2a51
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-26 11:49:43 +0000

    Define routes at index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,36 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/pilots',
+    name: 'Pilots',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "pilots" */ '../views/Pilots.vue')
+  },
+  {
+    path: '/pilot/:id',
+    name: 'Pilot',
+    component: Pilot,
+    props: {
+      id: '',
+    },
+  },
+  {
+    path: '/starships',
+    name: 'Starships',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "pilots" */ '../views/Pilots.vue')
+  },
+  {
+    path: '/starship/:id',
+    name: 'Starship',
+    component: Starship,
+    props: {
+      id: '',
+    },
   }
 ]
 
