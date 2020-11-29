f584ae0521a6b17c6c59d91402ed2f1f9f41df1c
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-29 11:04:17 +0000

    Get the number of items to display on Home Page
    with our swapiService

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="home">
     <h1>A list of all the  starships and  pilots featured on Star Wars films.</h1>
-    <div class="card">
-      <h2>{{ starshipscount }} starships</h2>
-    <router-link class="btn" to="/starships"> See all starships</router-link>
+    <div>
+    <div v-if="starshipscount>0"><h2>{{ starshipscount }}  starships</h2></div>
+    <div><router-link class="btn force" to="/starships"> See all starships</router-link></div>
     </div>
-    <div class="card">
-      <h2>{{ pilotscount }} pilots </h2>
-    <router-link class="btn" to="/pilots"> See all pilots</router-link>
-    </div>
+    <div>
+    <div v-if="pilotscount>0"><h2>{{ pilotscount }} pilots </h2></div>
+    <div><router-link class="btn force" to="/pilots"> See all pilots</router-link></div></div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from '@/components/HelloWorld.vue'
+  import SwApi from '@/services/api/SwApi.js'
+  const SwApiService = new SwApi();
+
 export default {
   name: 'Home',
   data () {
     return {
-    starshipscount: '-',
-    pilotscount: '-' 
+    starshipscount: 0,
+    pilotscount: 0 
     }
   },
-  mounted () {
-
-  },
-  components: {
+  async beforeMount () {
+    this.pilotscount = await SwApiService.getTotal('people')
+    this.starshipscount = await SwApiService.getTotal('starships')
+    window.scrollTo(0, 0)
   }
 }
 </script>
