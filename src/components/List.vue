7c08b304f6cbcc4668164dd7f6e50427fac5a55e
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-29 10:57:23 +0000

    Using new API (SwApi) service in List.js

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,44 +2,44 @@
   <div>
     <h1>{{ title }}</h1>
     <div class="card" v-for="item in items" :key="item.url">
-        <router-link :to="'/' + modelname + '/' + item.url.split('/')[5]">{{ item.name }}</router-link>
+      <router-link :to="'/' + modelname + '/' + item.url.split('/')[5]">{{ item.name }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+  import SwApi from '@/services/api/SwApi.js'
+  const SwApiService = new SwApi();
 
-export default {
-  name: 'List',
-  data: function () { 
-    return {
-      items: [],
-      nextavailable: 2 
+  export default {
+    name: 'List',
+    props: {
+      model: String,
+      modelname: String,
+      title: String
+    },
+    data: function () {
+      return {
+        items: [],
+        nextavailable: 2
       }
-  },
-  mounted () {
-    axios
-      .get('https://swapi.dev/api/'+this.model+'/')
-      .then(response => (this.items = response.data.results))
-  },
-  updated () {
-    if (this.nextavailable) 
-      this.getAll()
-  },
-  props: {
-    model: String,
-    modelname: String,
-    title: String
-  },
-  methods: {
-    getAll(){
-        axios
-          .get('https://swapi.dev/api/'+this.model+'/?page='+this.nextavailable)
-          .then(response => (
-            this.items = response.data.next != null ? this.items.concat(response.data.results) : this.items,
-            this.nextavailable = response.data.next != null ? this.nextavailable + 1 : false))
+    },
+    methods: {
+      async getMore() {
+        let data = await SwApiService.getNextPage(this.model, this.nextavailable)
+        this.items = this.items.concat(data.results)
+        this.nextavailable = data.next != null ? this.nextavailable + 1 : false
+      }
+    },
+    async mounted() {
+      this.items = await SwApiService.getFirstList(this.model);
+      this.nextavailable = 2;
+    },
+    updated() {
+      if (this.nextavailable) {
+        this.getMore()
       }
     }
   }
+
 </script>
