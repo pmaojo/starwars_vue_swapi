4b4d9867a5ab5540b9f0f5989569f57ce85b7f32
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-30 21:29:11 +0000

    using components on navbar

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,31 +1,21 @@
 <template>
   <div>
-    <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark">
-    <div class="container-fluid"><router-link to="/"><img alt="Vue logo" src="@/assets/img/logo.png"></router-link>
-    <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navcol-1">
-            <ul class="nav navbar-nav text-right text-white ml-auto">
-                <li class="nav-item" role="presentation"><router-link to="/" class="nav-link active">Home</router-link></li>
-                <li class="nav-item" role="presentation"><router-link
-            :class="$route.params.id && $route.name === 'Pilot' ? 'router-link-exact-active nav-link' : 'nav-link'"
-            to="/pilots/">Pilots</router-link></li>
-                <li class="nav-item" role="presentation"> <router-link
-            :class="$route.params.id && $route.name === 'Starship' ? 'router-link-exact-active nav-link' : 'nav-link'"
-            to="/starships/">Starships</router-link></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand to="/"><img alt="Vue logo" src="@/assets/img/logo.png"></b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/" class="home">Home</b-nav-item>
+          <b-nav-item to="/pilots/">Pilots</b-nav-item>
+          <b-nav-item to="/starships/">Starships</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 <script>
   export default {
-    props: {
-    },
-    data() {
-      return {
-      }
-    }
+    name: 'Menu'
   }
 
 </script>
