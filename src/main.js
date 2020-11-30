359edb4165905527fea7334537c4c0a2c3b8b5df
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-30 21:28:13 +0000

    bootstrap fix position of import

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.scss'
 
-
-Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
