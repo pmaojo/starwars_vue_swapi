d435805f4a6326c1369b64509dfa549a28c5b71e
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-30 15:30:00 +0000

    SCSS styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/main.scss'
+
 
 Vue.config.productionTip = false
 
@@ -8,3 +10,4 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
