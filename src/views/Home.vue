8e4cd16f396d388a1eebba0aea928aa776173a71
Pelayo Maojo <pelayo.mao@gmail.com>
2020-11-30 15:30:34 +0000

    Home grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="home">
-    <h1>A list of all the  starships and  pilots featured on Star Wars films.</h1>
-    <div>
-    <div v-if="starshipscount>0"><h2>{{ starshipscount }}  starships</h2></div>
-    <div><router-link class="btn force" to="/starships"> See all starships</router-link></div>
+  <div class="home container mt-5">
+    <div class="row">
+      <div class="col-5 offset-1">
+        <h1>A list of all the starships and pilots featured on Star Wars films.</h1>
+      </div>
+      <div v-if="starshipscount>0" class="offset-2 col-4">
+        <h2>{{ starshipscount }} starships</h2>
+        <router-link class="btn btn-primary btn-lg btn-success" to="/starships"> See all starships</router-link>
+      <div v-if="pilotscount>0" class="mt-4">
+        <h2>{{ pilotscount }} pilots </h2>
+        <div>
+          <router-link class="btn btn-primary btn-lg btn-success" to="/pilots"> See all pilots</router-link>
+        </div>
+      </div>
+      </div>
+
+
     </div>
     <div>
-    <div v-if="pilotscount>0"><h2>{{ pilotscount }} pilots </h2></div>
-    <div><router-link class="btn force" to="/pilots"> See all pilots</router-link></div></div>
+    </div>
   </div>
 </template>
 
@@ -15,24 +26,19 @@
   import SwApi from '@/services/api/SwApi.js'
   const SwApiService = new SwApi();
 
-export default {
-  name: 'Home',
-  data () {
-    return {
-    starshipscount: 0,
-    pilotscount: 0 
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        starshipscount: 0,
+        pilotscount: 0
+      }
+    },
+    async beforeMount() {
+      this.pilotscount = await SwApiService.getTotal('people')
+      this.starshipscount = await SwApiService.getTotal('starships')
+      window.scrollTo(0, 0)
     }
-  },
-  async beforeMount () {
-    this.pilotscount = await SwApiService.getTotal('people')
-    this.starshipscount = await SwApiService.getTotal('starships')
-    window.scrollTo(0, 0)
   }
-}
+
 </script>
-
-<style>
-  .home{max-width: 650px;
-  margin: auto;}
-
-</style>
